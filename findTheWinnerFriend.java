e3e1762f5e3181a3ef7345123b0c83113f6eaa9f
manisha-13-code <manishachauhan5469@gmail.com>
2025-01-01 18:15:52 +0530

    commit

---
+++ findTheWinnerFriend.java
@@ -0,0 +1,13 @@
+public class findTheWinnerFriend {
+    public int solve(int n, int k) {
+        if(n == 1) {
+           return 0; 
+       }
+         //%n -> (n-1)
+       return (solve(n-1, k) + k) % n;
+   }
+   public int findTheWinner(int n, int k) {
+       
+       return solve(n, k) + 1; 
+   }
+}
